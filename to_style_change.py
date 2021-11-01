ae9895fb3b4b4098d35c394587765b1a10155898
Thomas Hudson <07thomas.hudson@gmail.com>
2021-11-01 12:46:58 +0000

    Added 1 author docs

--- to_style_change.py
+++ to_style_change.py
@@ -6,6 +6,8 @@ import json
 import random
 import numpy as np
 
+from utils import MyEncoder, NoIndent
+
 def make_changes_list(paragraph_authors):
     '''Turns a list of author idx per para into a list of changes'''
     out = []
@@ -53,10 +55,10 @@ except:
     raise Exception("Style change already exists")
 
 #       Build map of fandom/author/paragraphs
-NUM_PER_SET = 100
+NUM_PER_SET = 999999
 random.seed(42)
 np.random.seed(42)
-BUFFER = 1000
+# BUFFER = 1000
 p_map = {}
 for fandom_path in glob.glob("fanfictions/*"):
     fandom = fandom_path.split("/")[-1]
@@ -76,64 +78,108 @@ for fandom_path in glob.glob("fanfictions/*"):
             raise Exception("No more authors left")
 
         length = random.randint(10000, 30000)
-        num_authors = random.randint(2, 3)
+        if random.random() <= 0.5:
+            num_authors = 1
+        else:
+            num_authors = random.randint(2, 4)
 
-        num_switches = random.randint(num_authors-1,10)
+        if num_authors == 1:
 
-        structure = createMixSenario(num_authors-1, num_switches)
-
-        # Work out how long each section should be
-        lengths = (np.random.dirichlet(np.ones(num_switches+1),size=1) * length)
-        lengths = list(lengths[0])
-
-        # Find length needed per author idx (e.g. author 1 needs 600 words...)
-        author_length_map = {}
-        for i in range(len(lengths)):
-            author_length_map[structure[i]] = int(author_length_map.get(structure[i], 0) + lengths[i])
-
-        # Pick authors
-        author_ids = []
-        paras_to_pick = {}
-        for i in set(structure):
+            # Find author with enough length
             new_author = None
             available_len = -1
-            while new_author in author_ids or available_len < author_length_map[i]:
+            while available_len < length:
                 new_author = random.sample(list(post_map.keys()), 1)[0]
-                available_len = len(" ".join(post_map[new_author]).split()) - BUFFER
-            author_ids.append(new_author)
+                available_len = len(" ".join(post_map[new_author]).split()) * 0.8
+            author_id = new_author
 
-        for author_id in author_ids:
-            paras_to_pick[author_id] = random.sample(post_map[author_id], len(post_map[author_id]))
+            post_map[author_id] = random.sample(post_map[author_id], len(post_map[author_id]))
 
-        # Construct document
-        paras = []
-        for i in range(len(structure)):
-            section = structure[i]
-            section_paras = []
-            while len("".join(section_paras).split()) < lengths[i]:
+            # Randomly pick paras
+            paras = []
+            while len("".join(paras).split()) < length:
                 try:
-                    section_paras.append(paras_to_pick[author_ids[section-1]].pop())
+                    section_paras.append(post_map[author_id].pop())
                 except Exception as e:
-                    curr_len = len("".join(section_paras).split())
-                    orig_available_len = len(" ".join(post_map[author_ids[section-1]]).split())
-                    print(e)
-                    breakpoint()
-            paras.append(section_paras)
+                    # curr_len = len("".join(section_paras).split())
+                    # orig_available_len = len(" ".join(post_map[author_ids[section-1]]).split())
+                    reject = True
+                    break
+                    # print(e)
+                    # breakpoint()
+            labels = {
+                "site": fandom.replace("*s*", "/"),
+                "authors": 1,
+                "structure": NoIndent([author_id]),
+                "multi-author": 0,
+                "changes": NoIndent([0] * len(paras)),
+                "paragraph-authors": NoIndent([1] * len(paras))
+            }
 
-        # Cleanup post_map
-        post_map = {k:v for (k,v) in post_map.items() if len(v) > 0}
-        # Output
-        paragraph_authors = make_paragraph_author_list(paras, structure)
+        else:
+            num_switches = random.randint(num_authors-1,10)
 
-        from utils import MyEncoder, NoIndent
-        labels = {
-            "site": fandom.replace("*s*", "/"),
-            "authors": num_authors,
-            "structure": NoIndent([author_ids[s-1] for s in structure]),
-            "multi-author": int(num_authors!=1),
-            "changes": NoIndent(make_changes_list(paragraph_authors)),
-            "paragraph-authors": NoIndent(paragraph_authors)
-        }
+            structure = createMixSenario(num_authors-1, num_switches)
+
+            # Work out how long each section should be
+            lengths = (np.random.dirichlet(np.ones(num_switches+1),size=1) * length)
+            lengths = list(lengths[0])
+
+            # Find length needed per author idx (e.g. author 1 needs 600 words...)
+            author_length_map = {}
+            for i in range(len(lengths)):
+                author_length_map[structure[i]] = int(author_length_map.get(structure[i], 0) + lengths[i])
+
+            # Pick authors
+            author_ids = []
+            paras_to_pick = {}
+            for i in set(structure):
+                new_author = None
+                available_len = -1
+                while new_author in author_ids or available_len < author_length_map[i]:
+                    new_author = random.sample(list(post_map.keys()), 1)[0]
+                    available_len = len(" ".join(post_map[new_author]).split()) * 0.8
+                author_ids.append(new_author)
+
+            # Shuffle
+            for author_id in author_ids:
+                post_map[author_id] = random.sample(post_map[author_id], len(post_map[author_id]))
+
+            # Construct document
+            reject = False
+            paras = []
+            for i in range(len(structure)):
+                section = structure[i]
+                section_paras = []
+                while len("".join(section_paras).split()) < lengths[i]:
+                    try:
+                        section_paras.append(post_map[author_ids[section-1]].pop())
+                    except Exception as e:
+                        curr_len = len("".join(section_paras).split())
+                        orig_available_len = len(" ".join(post_map[author_ids[section-1]]).split())
+                        reject = True
+                        break
+                        # print(e)
+                        # breakpoint()
+                paras.append(section_paras)
+
+            if reject:
+                continue
+
+            # Cleanup post_map
+            post_map = {k:v for (k,v) in post_map.items() if len(v) > 0}
+            # Output
+            paragraph_authors = make_paragraph_author_list(paras, structure)
+
+            labels = {
+                "site": fandom.replace("*s*", "/"),
+                "authors": num_authors,
+                "structure": NoIndent([author_ids[s-1] for s in structure]),
+                "multi-author": int(num_authors!=1),
+                "changes": NoIndent(make_changes_list(paragraph_authors)),
+                "paragraph-authors": NoIndent(paragraph_authors)
+            }
+
         base_dir = os.path.join("style_change", fandom)
         with open(os.path.join(base_dir, 'truth-problem-'+str(dataset_idx+1)+'.json'), 'w') as fp:
             fp.write(json.dumps(labels, cls=MyEncoder, sort_keys=False, indent=2))
@@ -141,4 +187,3 @@ for fandom_path in glob.glob("fanfictions/*"):
             for section in paras:
                 for para in section:
                     f.write(para.strip() + "\n")
-
